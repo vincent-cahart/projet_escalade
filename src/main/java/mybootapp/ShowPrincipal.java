ac88d0bef20bac8f1b9874d8a8816d5834cdb5df
a20034622 <127996378+a20034622@users.noreply.github.com>
2024-04-07 19:19:12 +0200

    Update ShowPrincipal.java
    optimisation

--- src/main/java/mybootapp/ShowPrincipal.java
+++ src/main/java/mybootapp/ShowPrincipal.java
@@ -8,16 +8,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller()
+/**
+ * Contrôleur pour afficher les informations du {@link Principal} actuellement authentifié.
+ * <p>
+ * Ce contrôleur répond aux requêtes sur le chemin "/principal" et renvoie une représentation textuelle
+ * de l'objet {@link Principal}, typiquement les détails de l'utilisateur authentifié.
+ * </p>
+ * 
+ * @author Votre Nom
+ */
+@Controller
 @RequestMapping("/principal")
 public class ShowPrincipal {
 
     protected final Log logger = LogFactory.getLog(getClass());
 
+    /**
+     * Gère la requête GET sur "/principal" et renvoie les informations du {@link Principal}.
+     * 
+     * @param p Le {@link Principal} injecté par Spring Security représentant l'utilisateur authentifié.
+     * @return La représentation textuelle de l'objet {@link Principal}.
+     */
     @ResponseBody
     @RequestMapping("")
     public String show(Principal p) {
-        logger.info("show user " + p);
+        logger.info("Affichage de l'utilisateur " + p);
         return p.toString();
     }
 
