56550567abe861db5ee4a7b3869f529c43a184be
a20034622 <127996378+a20034622@users.noreply.github.com>
2024-04-07 21:50:46 +0200

    Update SpringSecurity.java
    opt

--- src/main/java/mybootapp/security/SpringSecurity.java
+++ src/main/java/mybootapp/security/SpringSecurity.java
@@ -1,27 +1,3 @@
-package mybootapp.security;
-
-import java.util.List;
-
-import mybootapp.jpa.dao.repositeries.XUserRepository;
-import mybootapp.jpa.model.XUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.servlet.DispatcherType;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
@@ -30,6 +6,7 @@ public class SpringSecurity {
     @Autowired
     XUserRepository userRepo;
 
+    // Customise la configuration de sécurité web pour ignorer certains types de requêtes.
     @Bean
     WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> {
@@ -37,46 +14,35 @@ public class SpringSecurity {
         };
     }
 
+    // Configure la chaîne de filtres de sécurité pour définir les règles d'authentification et d'autorisation.
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        String[] anonymousRequests = { "/", //
-                "/webjars/**",
-        };
-        String[] adminRequests = { //
+        String[] anonymousRequests = { "/", "/webjars/**"};
+        String[] connectedRequests = {"/profil/**", "/sortie/modifier**"};
 
-        };
-        String[] connectedRequests = { //
-                "/profil/**", "/profil**",
-                "/sortie/modifier**", "/sortie/modifier**",
-        };
-
-        http.authorizeHttpRequests(config -> {//
+        http.authorizeHttpRequests(config -> {
             config.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll();
-            // Pour tous
-            config.requestMatchers(anonymousRequests).permitAll();//
-            config.requestMatchers(connectedRequests).authenticated();//
-            // Pour les admins
-            config.requestMatchers(adminRequests).hasAnyAuthority("ADMIN");//
-            // Pour les autres
+            config.requestMatchers(anonymousRequests).permitAll();
+            config.requestMatchers(connectedRequests).authenticated();
             config.anyRequest().permitAll();
         });
-        // Nous autorisons un formulaire de login
+
+        // Configure le formulaire de connexion et de déconnexion.
         http.formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
-        // Nous autorisons un formulaire de logout
         http.logout(config -> {
             config.permitAll();
             config.logoutSuccessUrl("/");
         });
-        // Nous activons CSRF pour les actions protégées
+
+        // Configure CSRF.
         http.csrf(config -> {
             config.ignoringRequestMatchers(anonymousRequests);
         });
+
         return http.build();
     }
 
-    /*
-     * Définition des utilisateurs en BD.
-     */
+    // Initialise la base de données avec des utilisateurs pour des tests ou un démarrage rapide de l'application.
     @PostConstruct
     public void init() {
         var encoder = passwordEncoder();
@@ -87,26 +53,18 @@ public class SpringSecurity {
         System.out.println("--- INIT SPRING SECURITY");
     }
 
-    
-
-    /*
-     * Définir le fournisseur d'authentification. Nous utilisons la version
-     * DaoAuthenticationProvider qui récupère les informations à partir du
-     * UserDetailsService que nous avons défini avant.
-     */
+    // Configure le fournisseur d'authentification pour utiliser le service UserDetailsService et le cryptage de mot de passe.
     @Bean
-    public AuthenticationProvider myAuthenticationProvider(//
-                                                           @Autowired PasswordEncoder encoder, //
-                                                           @Autowired UserDetailsService userDetailsService) {
+    public AuthenticationProvider myAuthenticationProvider(
+            @Autowired PasswordEncoder encoder, 
+            @Autowired UserDetailsService userDetailsService) {
         var authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(encoder);
         return authProvider;
     }
 
-    /*
-     * Définir la politique par défaut pour le cryptage des mots de passe.
-     */
+    // Définit la méthode de cryptage des mots de passe.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
