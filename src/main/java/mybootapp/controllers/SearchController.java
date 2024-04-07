aadbbd98d35fcd6ca8871f663c6e31fd1de06e6d
a20034622 <127996378+a20034622@users.noreply.github.com>
2024-04-07 21:34:32 +0200

    Update SearchController.java
    Optimisation

--- src/main/java/mybootapp/controllers/SearchController.java
+++ src/main/java/mybootapp/controllers/SearchController.java
@@ -1,6 +1,5 @@
 package mybootapp.controllers;
 
-
 import mybootapp.jpa.dao.JpaDao;
 import mybootapp.jpa.model.Sortie;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +10,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+/**
+ * Controller for handling search functionality within the application.
+ */
 @Controller
 public class SearchController {
 
     @Autowired
-    private JpaDao jpaDao;
+    private JpaDao jpaDao; // Injecting the DAO to access the database.
 
+    /**
+     * Handles search requests and displays the results.
+     * 
+     * @param keyword The search keyword provided by the user.
+     * @param model   The Model object to pass data to the view.
+     * @return The name of the view to render.
+     */
     @GetMapping("/search")
     public String search(@RequestParam String keyword, Model model) {
+        // Perform the search operation using the provided keyword.
         List<Sortie> searchResults = jpaDao.searchSortiesParCriteres(keyword);
+
+        // Add the search results to the model, making them accessible in the view.
         model.addAttribute("searchResults", searchResults);
-        return "search";
+
+        // Return the name of the view to render the search results.
+        return "search"; // This should correspond to the name of your search results page template.
     }
 }
